1720f1bfd2849a339433770ada2dc0a981b0c396
rupelio.c.junior <rupelio.c.junior@exxonmobil.com>
2023-03-16 16:56:34 -0300

    teste

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -4,4 +4,5 @@ from pathlib import Path
 class Parser:
     extensions = []
     def valid_extension(self, extension):
-        
+        if self.extensions in extension:
+            return extension
