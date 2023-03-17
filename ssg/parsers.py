0642d0b36a1b265d41011087f5cf8f73fbd11d89
rupelio.c.junior <rupelio.c.junior@exxonmobil.com>
2023-03-17 13:11:12 -0300

    Tudo

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -8,3 +8,7 @@ class Parser:
     
     def parse(self, path : Path, source : Path, dest : Path):
         raise NotImplementedError
+    
+    def read(self, path):
+        with open(path, "r") as file:
+            return read()
