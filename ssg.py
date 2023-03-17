523bc7515a7e48090d61a0d69696853387a7923f
rupelio.c.junior <rupelio.c.junior@exxonmobil.com>
2023-03-17 14:59:57 -0300

    Tudo

--- ssg.py
+++ ssg.py
@@ -1,14 +1,10 @@
 import typer
 from ssg.site import Site
 import ssg.parsers
-import sys
 
 def main(source = "content", dest = "dist"):
     config = {"source" : source, "dest" : dest, "parsers" : [ssg.parsers.ResourceParser(), ssg.parsers.MarkdownParser(), ssg.parsers.ReStructuredTextParser()]}
     site = Site(**config).build()
 
-@staticmethod
-def error(message):
-    sys.stderr.write("\x1b[1;31m{}\n").format(message)
 
 typer.run(main)
